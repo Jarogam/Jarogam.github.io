3f977b091a936119665e399d36238e7e2e63365a
Jarogam <alex.mayour2@gmail.com>
2019-10-21 22:11:48 +0300

    Add files via upload

---
+++ p4i/js/main.js
@@ -0,0 +1,15 @@
+$('.tabs').tabslet();
+
+jQuery(document).ready(function($) {
+
+    var menuToggle = function() {
+        $('.burger').click(function(e) {
+            $(this).toggleClass('open');
+            e.preventDefault();
+            $(this).toggleClass('is-active');
+        });
+    }
+
+    menuToggle();
+
+});
