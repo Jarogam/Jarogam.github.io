d20620f6ca1f27071e2f29fc9246ec71799042db
Jarogam <alex.mayour2@gmail.com>
2019-10-21 21:09:15 +0300

    Add files via upload

--- tvist/send.php
+++ tvist/send.php
@@ -1,66 +1,59 @@
 <?php
-//recipient
 
-$to = "Elena357910@yandex.com";  
+$to = 'Elena357910@yandex.com';
+
+if ( isset( $_POST['sendMail'] ) ) {
+  $email   = ( $_POST['email'], 0, 64 );
+  $phone = ( $_POST['phone'], 0, 64 );
+  $descr = ( $_POST['descr'], 0, 250 );
+//   $message = substr( $_POST['message'], 0, 250 );
 
 
-$subject = "Форма отправки сообщений с сайта Твист";
-    
-//sender
-$from = "Elena357910@yandex.com"; 
-$fromName = 'Tvist';
-
-//email subject
-$subject = 'PHP Email with Attachment by CodexWorld'; 
-
-//attachment file path
-$email = $_POST['email'];
-$phone = $_POST['phone'];
-$descr = $_POST['descr'];
-$file = $_FILES['file'];
-
-//email body content
-$htmlContent = '<h1>PHP Email with Attachment by CodexWorld</h1>
-    <p>This email has sent from PHP script with attachment.</p>';
-
-//header for sender info
-$headers = "From: $fromName"." <".$from.">";
-
-//boundary 
-$semi_rand = md5(time()); 
-$mime_boundary = "==Multipart_Boundary_x{$semi_rand}x"; 
-
-//headers for attachment 
-$headers .= "\nMIME-Version: 1.0\n" . "Content-Type: multipart/mixed;\n" . " boundary=\"{$mime_boundary}\""; 
-
-//multipart boundary 
-$message = "--{$mime_boundary}\n" . "Content-Type: text/html; charset=\"UTF-8\"\n" .
-"Content-Transfer-Encoding: 7bit\n\n" . $htmlContent . "\n\n"; 
-
-//preparing attachment
-if(!empty($file) > 0){
-    if(is_file($file)){
-        $message .= "--{$mime_boundary}\n";
-        $fp =    @fopen($file,"rb");
-        $data =  @fread($fp,filesize($file));
-
-        @fclose($fp);
-        $data = chunk_split(base64_encode($data));
-        $message .= "Content-Type: application/octet-stream; name=\"".basename($file)."\"\n" . 
-        "Content-Description: ".basename($file)."\n" .
-        "Content-Disposition: attachment;\n" . " filename=\"".basename($file)."\"; size=".filesize($file).";\n" . 
-        "Content-Transfer-Encoding: base64\n\n" . $data . "\n\n";
-    }
+  if ( !empty( $_FILES['file']['tmp_name'] ) and $_FILES['file']['error'] == 0 ) {
+    $filepath = $_FILES['file']['tmp_name'];
+    $filename = $_FILES['file']['name'];
+  } else {
+    $filepath = '';
+    $filename = '';
+  }
+ 
+  $body = "Контактный номер:\r\n".$phone."\r\n\r\n";
+  $body .= "E-mail:\r\n".$email."\r\n\r\n";
+  $body .= "Описание заказа:\r\n".$descr;
+ 
+  send_mail($to, $body, $email, $filepath, $filename);
 }
-$message .= "Email: ". $email; 
-$message .= "Телефон: ". $phone; 
-$message .= "Сообщение: ". $descr; 
-$message .= "--{$mime_boundary}--";
-$returnpath = "-f" . $from;
 
-//send email
-$mail = @mail($to, $subject, $message, $headers, $returnpath); 
+// Вспомогательная функция для отправки почтового сообщения с вложением
+function send_mail($to, $body, $email, $filepath, $filename)
+{
+  $subject = 'Заказ с сайта Tvist';
+  $boundary = "--".md5(uniqid(time())); // генерируем разделитель
+  $headers = "From: ".$email."\r\n";   
+  $headers .= "MIME-Version: 1.0\r\n";
+  $headers .="Content-Type: multipart/mixed; boundary=\"".$boundary."\"\r\n";
+  $multipart = "--".$boundary."\r\n";
+  $multipart .= "Content-type: text/plain; charset=\"utf-8\"\r\n";
+  $multipart .= "Content-Transfer-Encoding: quoted-printable\r\n\r\n";
 
-//email sending status
-echo $mail?"<h1>Mail sent.</h1>":"<h1>Mail sending failed.</h1>";
+  $body = $body."\r\n\r\n";
+ 
+  $multipart .= $body;
+ 
+  $file = '';
+  if ( !empty( $filepath ) ) {
+    $fp = fopen($filepath, "r");
+    if ( $fp ) {
+      $content = fread($fp, filesize($filepath));
+      fclose($fp);
+      $file .= "--".$boundary."\r\n";
+      $file .= "Content-Type: application/octet-stream\r\n";
+      $file .= "Content-Transfer-Encoding: base64\r\n";
+      $file .= "Content-Disposition: attachment; filename=\"".$filename."\"\r\n\r\n";
+      $file .= chunk_split(base64_encode($content))."\r\n";
+    }
+  }
+  $multipart .= $file."--".$boundary."--\r\n";
+  mail($to, $subject, $multipart, $headers);
+}
 ?>
