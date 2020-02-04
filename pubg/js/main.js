b2fba437a06747ce4db3c76648f7a8211be97da4
Jarogam <alex.mayour2@gmail.com>
2020-02-04 18:07:45 +0200

    fixed bugs
    fixed bugs and added popup

--- pubg/js/main.js
+++ pubg/js/main.js
@@ -1,97 +1,96 @@
+$('#popup1').popup();
 
 
-
-// let burger = document.getElementsByClassName("body");
 function miniburger() {
-    var element = document.querySelector("body");
-    // element.classList.add("sidebar-mini");
+    let element = document.querySelector("body");
     element.classList.toggle("sidebar-mini");
   }
 
-
-// var swiper = new Swiper('.swiper-tournaments', {
-//     slidesPerView: 3,
-//     spaceBetween: 30,
-//     navigation: {
-//         nextEl: ".swiper-tournaments-button-next",
-//         prevEl: ".swiper-tournaments-button-prev"
-//     },
-//     breakpoints: {
-//         // when window width is >= 320px
-//         320: {
-//           slidesPerView: 1,
-//           spaceBetween: 20
-//         },
-//         // when window width is >= 480px
-//         480: {
-//           slidesPerView: 1,
-//           spaceBetween: 40
-//         },
-//         // when window width is >= 640px
-//         576: {
-//           slidesPerView: 1,
-//           spaceBetween: 40
-//         },
-//         768: {
-//           slidesPerView: 1,
-//           spaceBetween: 40
-//         },
-//         992: {
-//           slidesPerView: 1,
-//           spaceBetween: 40
-//         },
-//         1200: {
-//           slidesPerView: 2,
-//           spaceBetween: 20
-//         },
-//         // 1400: {
-//         //   slidesPerView: 2,
-//         //   spaceBetween: 20
-//         // }
+var swiper = new Swiper('.swiper-tournaments', {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    navigation: {
+        nextEl: ".swiper-tournaments-button-next",
+        prevEl: ".swiper-tournaments-button-prev"
+    },
+    breakpoints: {
+        // when window width is >= 320px
+        320: {
+          slidesPerView: 1,
+          spaceBetween: 20
+        },
+        // when window width is >= 480px
+        480: {
+          slidesPerView: 1,
+          spaceBetween: 40
+        },
+        // when window width is >= 640px
+        576: {
+          slidesPerView: 1,
+          spaceBetween: 40
+        },
+        768: {
+          slidesPerView: 1,
+          spaceBetween: 40
+        },
+        992: {
+          slidesPerView: 1,
+          spaceBetween: 40
+        },
+        1200: {
+          slidesPerView: 2,
+          spaceBetween: 20
+        },
+        // 1400: {
+        //   slidesPerView: 2,
+        //   spaceBetween: 20
+        // }
         
-//       }
-// });
-// var swiper = new Swiper('.swiper-award', {
-//     slidesPerView: 2,
-//     spaceBetween: 30,
-//     navigation: {
-//         nextEl: ".swiper-award-button-next",
-//         prevEl: ".swiper-award-button-prev"
-//     },
-//     breakpoints: {
-//         // when window width is >= 320px
-//         320: {
-//           slidesPerView: 1,
-//           spaceBetween: 20
-//         },
-//         // when window width is >= 480px
-//         480: {
-//           slidesPerView: 1,
-//           spaceBetween: 40
-//         },
-//         // when window width is >= 640px
-//         576: {
-//           slidesPerView: 1,
-//           spaceBetween: 40
-//         },
-//         768: {
-//           slidesPerView: 1,
-//           spaceBetween: 40
-//         },
-//         992: {
-//           slidesPerView: 1,
-//           spaceBetween: 20
-//         },
-//         1200: {
-//           slidesPerView: 1,
-//           spaceBetween: 40
-//         },
-//         // 1400: {
-//         //   slidesPerView: 1,
-//         //   spaceBetween: 20
-//         // }
-        
-//       }
-// });
+      }
+});
 
-$('.tabs').tabslet();
+var swiper = new Swiper('.swiper-award', {
+    slidesPerView: 2,
+    spaceBetween: 30,
+    navigation: {
+        nextEl: ".swiper-award-button-next",
+        prevEl: ".swiper-award-button-prev"
+    },
+    breakpoints: {
+        // when window width is >= 320px
+        320: {
+          slidesPerView: 1,
+          spaceBetween: 20
+        },
+        // when window width is >= 480px
+        480: {
+          slidesPerView: 1,
+          spaceBetween: 40
+        },
+        // when window width is >= 640px
+        576: {
+          slidesPerView: 1,
+          spaceBetween: 40
+        },
+        768: {
+          slidesPerView: 1,
+          spaceBetween: 40
+        },
+        992: {
+          slidesPerView: 1,
+          spaceBetween: 20
+        },
+        1200: {
+          slidesPerView: 1,
+          spaceBetween: 40
+        },
+        // 1400: {
+        //   slidesPerView: 1,
+        //   spaceBetween: 20
+        // }
+        
+      }
+});
+
+
+
