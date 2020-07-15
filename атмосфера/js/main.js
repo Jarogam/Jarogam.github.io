3ef7c3a475bf8ed30f8c096f7efa6916cdcf10de
Jarogam <alex.mayour2@gmail.com>
2020-07-15 10:48:20 +0300

    home page fixed

---
+++ атмосфера/js/main.js
@@ -0,0 +1,23 @@
+// var instance = M.Carousel.init({
+//     fullWidth: true
+//   });
+
+  // Or with jQuery
+
+//   $('.carousel.carousel-slider').carousel({
+//     fullWidth: true,
+//     autoplay: true
+//   });
+// autoplay();
+// function autoplay() {
+//     $('.carousel.carousel-slider').carousel('next');
+//     setTimeout(autoplay, 4500);
+// }
+
+$('.carousel.carousel-slider').carousel({
+        fullWidth: true
+}
+);
+setInterval(function() {
+  $('.carousel.carousel-slider').carousel('next');
+}, 3500); // every 5 seconds
